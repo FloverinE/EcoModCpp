bad0f71c48161623f2ef4d26202cb7f80887d561
florian.wilms01 <florian.wilms01@stud.uni-goettingen.de>
2024-02-25 15:11:15 +0100

    works again, added vector of tree objects setup trees now clears all tree objects to initialize

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -47,13 +47,18 @@ void MainWindow::on_setup_button_clicked()
 }
 
 std::vector<int> tree_ids;
+
+std::vector<tree> trees;
+
 void MainWindow::setup_trees() {
+    trees.clear();
     int N_trees = ui->N_trees_spinBox->value();
     double species_ratio = ui->species_ratio_spinBox->value();
     int N_birch_trees = N_trees * species_ratio;
 //    int N_oak_trees = N_trees - N_birch_trees; // not used since if_else
-    tree trees[N_trees];
+//    tree trees[N_trees];
     for (int i = 0; i < N_trees; ++i) {
+        trees.emplace_back(i, std::vector<int>{}, 'b', 1, 10);
         trees[i].x_y_cor = {rand() % xSize, rand() % ySize};
         if (i <= N_birch_trees){
             trees[i].species = 'b';
@@ -67,33 +72,35 @@ void MainWindow::setup_trees() {
         trees[i].update_species_params();
     }
 
-    cout << N_trees << endl;
+    for (const auto& t : trees) {
+        std::cout << "Tree ID: " << t.id << ", Species: " << t.species << std::endl;
+    }
     scene->addPixmap(QPixmap::fromImage(image));
 
 }
 
-std::vector<tree> trees;
 void MainWindow::perform_dispersal() {
-    //write an auto loop for tree_ids
-    for (const auto& tree_id : tree_ids) {
-//        if (tree.id == tree_id) {
-            int real_seed_production = trees[tree_id].max_seed_production / 50;
-            std::cout << real_seed_production << std::endl;
-            for (int i = 0; i < real_seed_production; ++i) {
-                double direction = 2 * M_PI * i / real_seed_production;
-                int offset_x = trees[i].dispersal_factor * cos(direction);
-                int offset_y = trees[i].dispersal_factor * sin(direction);
-                image.setPixel(trees[i].x_y_cor[0] + offset_x, trees[i].x_y_cor[1] + offset_y, color_saplings);
-            }
-//        }
-        std::cout << "Tree ID: " << tree_id << std::endl;
-    }
-    scene->addPixmap(QPixmap::fromImage(image));
+    std::cout << trees[1].max_seed_production << std::endl;
+
+//    for (const auto& tree_id : tree_ids) {
+////        if (tree.id == tree_id) {
+//            int real_seed_production = trees[tree_id].max_seed_production / 50;
+//            std::cout << real_seed_production << std::endl;
+//            for (int i = 0; i < real_seed_production; ++i) {
+//                double direction = 2 * M_PI * i / real_seed_production;
+//                int offset_x = trees[i].dispersal_factor * cos(direction);
+//                int offset_y = trees[i].dispersal_factor * sin(direction);
+//                image.setPixel(trees[i].x_y_cor[0] + offset_x, trees[i].x_y_cor[1] + offset_y, color_saplings);
+//            }
+////        }
+//        std::cout << "Tree ID: " << tree_id << std::endl;
+//    }
+//    scene->addPixmap(QPixmap::fromImage(image));
 }
 
 void MainWindow::on_go_button_clicked()
 {
-    perform_dispersal();
+//    perform_dispersal();
 }
 
 MainWindow::~MainWindow()
