a1b560d4fed56f5f17784ada3fb6ec6f631f4e92
florian.wilms01 <florian.wilms01@stud.uni-goettingen.de>
2024-02-25 13:51:16 +0100

    put patch code from old project into class files

--- patch.cpp
+++ patch.cpp
@@ -1,6 +1,25 @@
 #include "patch.h"
+#include <vector>
 
-patch::patch()
+patch::patch(){}
+
+patch::patch(std::vector<int> x_y_cor,
+             int N_seedlings,
+             int dist_to_tree)
 {
+    this -> x_y_cor;
+    this -> N_seedlings;
+    this -> dist_to_tree;
+}
 
+patch::update_N_seedlings(int count) {
+    N_seedlings += count;
+}
+
+patch::get_seedlings(){
+    return N_seedlings;
+}
+patch::get_coords(){
+    return x_y_cor[0];
+    //    return x_y_cor[1];
 }
--- patch.h
+++ patch.h
@@ -1,11 +1,25 @@
 #ifndef PATCH_H
 #define PATCH_H
+#include <vector>        // package to use vectors
 
 
 class patch
 {
 public:
     patch();
+
+    patch(std::vector<int> x_y_cor,
+          int N_seedlings,
+          int dist_to_tree);
+    get_seedlings();
+    update_N_seedlings(int count);
+    get_coords();
+    std::vector<int> x_y_cor;
+
+private:
+    int N_seedlings = 0;
+    int dist_to_tree;
+
 };
 
 #endif // PATCH_H
